5cb0306896994f7b8248da6c5acd3a29ed6dbf67
Alae Ez-zaouine <aez-zaou@student.1337.ma>
2021-05-29 18:35:17 +0100

    sorting algorithm is working well, cleaning...

--- new_push/quick_sort.c
+++ new_push/quick_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quick_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alae <alae@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aez-zaou <aez-zaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 15:43:38 by aez-zaou          #+#    #+#             */
-/*   Updated: 2021/05/29 00:32:54 by alae             ###   ########.fr       */
+/*   Updated: 2021/05/29 18:32:33 by aez-zaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		pick_pivot(t_data data, int start, int end)
 		i++;
 	}
 	sort_arr(arr, size);
-	i = arr[(int)(size / 2)];
+	i = arr[(size / 2)];
 	free(arr);
 	return (i);
 }
@@ -55,7 +55,7 @@ int    push_chunk(t_chunk **head, int start, int end, int i)
 		return (ERROR);
     new_chunk->index[0] = start;
     new_chunk->index[1] = end;
-	if (i == SORTED)
+	if (i)
    		new_chunk->status = SORTED;
 	else
    		new_chunk->status = UNSORTED;
@@ -71,11 +71,6 @@ int    initialise_chunks(t_data data, t_chunk **a, t_chunk **b)
 	*a = NULL;
     if (!(push_chunk(a, 0, data.size, UNSORTED)))
 		return (ERROR);
-	// while (a)
-	// {
-	// 	printf("index[0] = %d\nindex[1] = %d\nstatus = %d\n\n", a->index[0], a->index[1], a->status);
-	// 	a = a->next;
-	// }
 	return (SUCCESS);
 }
 
@@ -111,14 +106,8 @@ int		*copyAndSort(t_data *data)
 		dst[i] = data->stack[i];
 		i++;
 	}
+	//TODO: no need to copy and sort the array;
 	sort_arr(dst, data->size);
-	// i = 0;
-	// while (i < data->size)
-	// {
-	// 	printf("|%d|", dst[i]);
-	// 	i++;
-	// }
-	// printf("\n");
 	return (dst);
 }
 
@@ -132,8 +121,6 @@ void	b_to_a(t_data *data, t_chunk **a, t_chunk **b)
 
 	start = (*a)->index[0];
 	end = (*a)->index[1] - 1;
-	size = end - start; // size , max num of operations we will do
-	pivot = pick_pivot(*data, start, end);
 	int i = UNSORTED;
 	if (end - start == 1)
 	{
@@ -154,15 +141,12 @@ void	b_to_a(t_data *data, t_chunk **a, t_chunk **b)
 			free(*a);
 			*a = NULL;
 		}
-		/// todo: free chunk
 	}
 	else if (end - start == 0)
 	{	
 		pa_(data);
-		i = SORTED;
-		(*a)->status = SORTED;
 		start++;
-
+		push_chunk(b, (*b)->index[1], data->b_index, SORTED);
 		if ((*a)->next)
 		{
 			tmp = *a;
@@ -174,9 +158,13 @@ void	b_to_a(t_data *data, t_chunk **a, t_chunk **b)
 			free(*a);
 			*a = NULL;
 		}
+		return ;
 	}
 	else {
 		int i = 0;
+		pivot = pick_pivot(*data, (*a)->index[0], (*a)->index[1]);
+		// size = end - start; // size , max num of operations we will do
+		size = (*a)->index[1] - (*a)->index[0];
 		while (size)
 		{
 			if (!(*a)->next && data->stack[start] > pivot && data->stack[end] > pivot)
@@ -199,7 +187,7 @@ void	b_to_a(t_data *data, t_chunk **a, t_chunk **b)
 				pa_(data);
 				start++;
 			}
-			else if (data->stack[start] > pivot)
+			if (data->stack[start] > pivot)
 			{
 				pa_(data);
 				start++;
@@ -210,23 +198,14 @@ void	b_to_a(t_data *data, t_chunk **a, t_chunk **b)
 				i++;
 			}
 			size--;
-			
-
-			
-			// print_A(*data);
-			// print_B(*data);
-			// print_stack(*data);
 		}
 		while ((i--))
 			rrb_(data);
 	}
-
 	if (*a)
 		(*a)->index[0] = start;
 	if (*b)
 		push_chunk(b, (*b)->index[1], data->b_index, i);
-	// else
-	// 	push_chunk(b, data->b_index, data->size);
 }
 
 void	a_to_b(t_data *data, t_chunk **a, t_chunk **b)
@@ -238,29 +217,28 @@ void	a_to_b(t_data *data, t_chunk **a, t_chunk **b)
 
 	start = (*a)->index[0];
 	end = (*a)->index[1] - 1;
-	size = end - start + 1; // size , max num of operations we will do
-	pivot = pick_pivot(*data, start, end);
 	int i = UNSORTED;
 	if (end - start == 1)
 	{
-		sa_(data);
-		i = SORTED;
-		// return ;
+		if (data->stack[start] < data->stack[end])
+			sa_(data);
+		(*a)->status = 1;
+		return ;
 	}
-	// printf("| 0 | 1 | 2 | 3 | 4 | 5 |\n");
-	// int i = 0;
-	// while (i < data->size)
-	// {
-	// 	printf("| %d ", data->stack[i]);
-	// 	i++;
-	// }
-	// printf("|\n");
+	else if (end - start == 0)
+	{
+		(*a)->status = 1;
+		return ;
+	}
 	else
 	{
 		int i = 0;
-		while (--size)
+		pivot = pick_pivot(*data, (*a)->index[0], (*a)->index[1]);	
+		// size = end - start + 1; // size , max num of operations we will do
+		size = (*a)->index[1] - (*a)->index[0];
+		while (size)
 		{
-			if (!(*a)->next && data->stack[start] <= pivot && data->stack[end] <= pivot)
+			if (!(*a)->next && data->stack[start] < pivot && data->stack[end] < pivot)
 			{
 				if (data->stack[end] < data->stack[start])
 				{
@@ -274,13 +252,13 @@ void	a_to_b(t_data *data, t_chunk **a, t_chunk **b)
 					end--;
 				}
 			}
-			else if (!(*a)->next && data->stack[start] <= pivot)
+			else if (!(*a)->next && data->stack[start] < pivot)
 			{
 				rra_(data);
 				pb_(data);
 				end--;
 			}
-			else if (data->stack[end] <= pivot)
+			if (data->stack[end] < pivot)
 			{
 				pb_(data);
 				end--;
@@ -290,39 +268,16 @@ void	a_to_b(t_data *data, t_chunk **a, t_chunk **b)
 				ra_(data);
 				i++;
 			}
-			// TODO: if here is only one chunk, no need to reverse rotate
-			
-				
-			// print_A(*data);
-			// print_B(*data);
-			// print_stack(*data);
+			size--;
 		}
 		while ((i--))
 			rra_(data);
 	}
-		
 	(*a)->index[1] = end + 1;
-	
 	if (*b)
 		push_chunk(b, data->b_index, (*b)->index[0], i);
 	else
 		push_chunk(b, data->b_index, data->size, i);
-	
-
-	// --- 
-	// i = 0;
-	// while (i < data->size)
-	// {
-	// 	printf("%d -> ", data->stack[i]);
-	// 	// printf("index[0] = %d\nindex[1] = %d\nstatus = %d\nPIVOT : %D\n\n", A_chunks->index[0], A_chunks->index[1], A_chunks->status, pivot);
-	// 	i++;
-	// }
-	// printf("\n");
-	// ---
-	// print_A(*data);
-	// print_B(*data);
-	// print_stack(*data);
-
 }
 
 int quick_sort(t_data *data)
@@ -334,41 +289,20 @@ int quick_sort(t_data *data)
     if (!initialise_chunks(*data, &A_chunks, &B_chunks) ||
 		!(sorted_stack = copyAndSort(data)))
 		return (ERROR);
-	// chunk a is invalid after initialising itttt
-	
-	// int pivot = pick_pivot(*data, 0, data->size);
-
     while (1)
     {
-        // if (is_chunk_sorted(*data, A_chunks->index[0], A_chunks->index[1]) && !B_chunks)
         if (A_chunks->status == SORTED && !B_chunks)
             break ;
-        // else if (is_chunk_sorted(*data, A_chunks->index[0], A_chunks->index[1]))
         else if (A_chunks->status == SORTED)
         {
-			if (A_chunks)
-				printf("A ==> index[0] = %d --- index[1] = %d \n",  A_chunks->index[0], A_chunks->index[1]);
-			if (B_chunks)
-				printf("B ==> index[0] = %d --- index[1] = %d \n",  B_chunks->index[0], B_chunks->index[1]);
             b_to_a(data, &B_chunks, &A_chunks);
         }
         else
         {
-			if (A_chunks)
-				printf("A ==> index[0] = %d --- index[1] = %d \n",  A_chunks->index[0], A_chunks->index[1]);
-			if (B_chunks)
-				printf("B ==> index[0] = %d --- index[1] = %d \n",  B_chunks->index[0], B_chunks->index[1]);
             a_to_b(data, &A_chunks, &B_chunks);
         }
-		// print_A(*data);
-		// print_B(*data);
-		print_stack(*data);
-		sleep(3);
-		
     }
-	// 
-	
-	
+	// print_stack(*data);
 	return (SUCCESS);
 
 }
@@ -377,3 +311,5 @@ int quick_sort(t_data *data)
 
 
 // last error : ./a.out 6 5 4 8 9 3 -8 0 7 1 2  --> see screenshot
+
+// TODO: no need sort the stack stack in another array
